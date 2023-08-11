b957de28cb81002348a6cf60362e75b8d7cc73c2
Praveer Panghal <admin@spinoinc.com>
2023-08-11 16:09:13 -0400

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './App.css';
+
+// Import your components for different pages
+import Home from './components/Home';
+import Services from './components/Services';
+import ContactUs from './components/ContactUs';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-       <h1>  Welcome to OjaArv.com</h1>
-       
-        
-      </header>
-    </div>
+    <Router basename="/ojaarv-app"> 
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+            <li>
+              <Link to="/contactus">Contact Us</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/services">
+            <Services />
+          </Route>
+          <Route path="/contactus">
+            <ContactUs />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/ContactUs.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ContactUs() {
+  return <h2>Contact Us</h2>;
+}
+
+export default ContactUs;
