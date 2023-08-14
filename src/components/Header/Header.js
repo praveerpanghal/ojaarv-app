cc2271180bc807aa6188bc299ba6c4c7eb2463d9
Praveer Panghal <admin@spinoinc.com>
2023-08-13 20:42:30 -0400

    .

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,22 +3,30 @@ import { Link, Routes, Route } from 'react-router-dom';
 import Home from '../Home/Home';
 import Services from '../Services/Services';
 import ContactUs from '../ContactUs/ContactUs';
+import 'bootstrap/dist/css/bootstrap.css'; // Import Bootstrap CSS
 
 function Header() {
   return (
     <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/services">Services</Link>
-          </li>
-          <li>
-            <Link to="/contactus">Contact Us</Link>
-          </li>
-        </ul>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/"><i>OjaArv</i></Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/services">Services</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contactus">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
